f450570734226d52ea549e7ad9a4702d5e727b53
Engg <enzo.9354@colegioprogresso.g12.br>
2026-05-21 12:08:40 -0300

    adionamo evento no botao

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const display = document.getElementById('display')
 
 function inserirTarefa(){
     let tarefa = input.value
+
+    if(tarefa == ''){
+         alert('Favor, insira uma ')
+    }
+
     display.innerHTML += 
     `<div class="tarefa">
         ${tarefa}
@@ -16,3 +21,9 @@ function inserirTarefa(){
 }
 
 button.addEventListener("click", inserirTarefa)
+
+display.addEventListener("click", function(event){
+    if(event.target.classList.contains('delete')){
+        event.target.parentElement.parentElement.remove()
+    }
+})
